0d3859741c75fbead170b870f1f6932b5fbdfd35
Muhammad Asif <m.asif@live.dk>
2017-09-24 20:30:20 +0200

    Added code for Adapter pattern

---
+++ AdapterPattern/AdapterPattern/VenderAdapter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdapterPattern
+{
+   public class VendorAdapter : ITarget
+    {
+        public List<string> GetProducts()
+        {
+            VendorAdaptee adaptee = new VendorAdaptee();
+            return adaptee.GetListOfProducts();
+        }
+    }
+
+}
---
+++ AdapterPattern/TestAdapter/UnitTest1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AdapterPattern;
+
+namespace TestAdapter
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            ITarget adapter = new VendorAdapter();
+
+            var products = adapter.GetProducts();
+
+            //4 items
+            Assert.IsNotNull(products);
+                        
+        }
+    }
+}
